ce65fd7de1ff582d3dbe05f447ae68743f292444
Promit-revar <promit.revar2211@gmail.com>
2023-03-09 23:43:43 +0530

    Feat: add navComponent

---
+++ src/components/navComponent/index.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable */
+import React from "react";
+import "./navComponent.css";
+
+export default function NavComponent() {
+    return (
+        <nav>
+            <div className="nav-container">
+                <div className="nav-logo">
+                    <h1>
+                        Content Types
+                    </h1>
+                </div>
+            </div>
+        </nav>
+    );
+}
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,9 +1,12 @@
+/* eslint-disable */
 import React from "react";
 import "./HomePage.css";
+import NavComponent from "../../components/navComponent";
 
 export default function HomePage() {
     return (
         <div className="HomePage">
+            <NavComponent />
         <h1>Home Page</h1>
         </div>
     );
