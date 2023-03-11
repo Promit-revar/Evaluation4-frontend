49a4af36c73c1e9a9708074ae7e28cca7b511e89
Promit-revar <promit.revar2211@gmail.com>
2023-03-11 22:20:46 +0530

    feat: removed protected route

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
-import ProtectedRoute from './utils/protectedRoute';
 import { LOGIN_ROUTE, HOME_ROUTE } from './constants/routes';
 
 export default function App() {
@@ -10,9 +9,9 @@ export default function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <ProtectedRoute>
+         
             <Route path={HOME_ROUTE} element={<HomePage />} />
-          </ProtectedRoute>
+          
           <Route path={LOGIN_ROUTE} element={<LoginPage />} />
         </Routes>
       </BrowserRouter>
--- src/utils/__tests__/makeRequest.test.js
+++ src/utils/__tests__/makeRequest.test.js
@@ -1,12 +1,13 @@
-import makeRequest from "../makeRequest";
-describe("makeRequest", () => {
-    it("should make request", async () => {
-        const result = await makeRequest({ url: "url", method: "method" });
-        expect(result).toEqual("result");
-    });
-    it("should throw error", async () => {
-        expect(makeRequest({ url: "url", method: "method" })).rejects.toThrow(
-        "error"
-        );
-    });
-    });
+import makeRequest from '../makeRequest';
+
+describe('makeRequest', () => {
+  it('should make request', async () => {
+    const result = await makeRequest({ url: 'url', method: 'method' });
+    expect(result).toEqual('result');
+  });
+  it('should throw error', async () => {
+    expect(makeRequest({ url: 'url', method: 'method' })).rejects.toThrow(
+      'error',
+    );
+  });
+});
